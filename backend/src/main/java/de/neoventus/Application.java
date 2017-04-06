6a20bc9bc9fed4f0368430ca38a6534493018b23
Dennis Thanner <dennis-thanner@web.de>
2017-04-06 18:32:17 +0200

    added default application class and pom libraries

---
+++ backend/src/main/java/de/neoventus/Application.java
@@ -0,0 +1,19 @@
+package de.neoventus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * default Spring entry class
+ *
+ * @author Dennis Thanner
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+}
