482a57626189fa3fe291e76978083098cefa57fd
Dennis Thanner <dennis-thanner@web.de>
2017-04-09 12:47:20 +0200

    added user repository unit test

---
+++ backend/src/test/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+import de.neoventus.persistence.entity.User;
+import de.neoventus.persistence.repository.UserRepository;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * testing the user repository methods
+ *
+ * @author Dennis Thanner
+ * @version 0.0.1
+ **/
+public class UserRepositoryTest extends AbstractTest {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+	public void testSearchByName() {
+		User u = new User();
+		u.setUsername("Test 1");
+		
+		userRepository.save(u);
+		
+		u = userRepository.findByUsername("Test 1");
+		
+		Assert.assertNotNull(u);
+		
+		Assert.assertTrue(u.getUsername().equals("Test 1"));
+	}
+	
+	/**
+	 * clear the data written
+	 */
+	@After
+	public void deleteAll() {
+		userRepository.deleteAll();
+	}
+	
+}
