0d825ac715d36cffa7e6f7d6bc21a06f1691e329
julian <julianlevinbeck@gmail.com>
2017-04-28 00:05:16 +0200

    Merge branch 'master' of https://bitbucket.org/timperator/neoventus
    # Conflicts:
    #	backend/src/main/java/de/neoventus/persistence/repository/advanced/impl/OrderItemRepositoryImpl.java

--- backend/src/main/java/de/neoventus/init/DefaultDemoDataIntoDB.java
+++ backend/src/main/java/de/neoventus/init/DefaultDemoDataIntoDB.java
@@ -23,6 +23,7 @@ class DefaultDemoDataIntoDB {
 	private static final int MAX_DESKS = 10;
 	private final static Logger LOGGER = Logger.getLogger(DefaultDemoDataIntoDB.class.getName());
 	private MenuItemRepository menuItemRepository;
+	private MenuItemCategoryRepository menuItemCategoryRepository;
 	private UserRepository userRepository;
 	private DeskRepository deskRepository;
 	private OrderItemRepository orderItemRepository;
@@ -35,10 +36,11 @@ class DefaultDemoDataIntoDB {
 	private List<User> users;
 
 	public DefaultDemoDataIntoDB(DeskRepository deskRepository, UserRepository userRepository,
-								 MenuItemRepository menuItemRepository, OrderItemRepository orderItemRepository,
+								 MenuItemRepository menuItemRepository, MenuItemCategoryRepository menuItemCategoryRepository, OrderItemRepository orderItemRepository,
 								 ReservationRepository reservationRepository, BillingRepository billingRepository) {
 
 		this.menuItemRepository = menuItemRepository;
+		this.menuItemCategoryRepository = menuItemCategoryRepository;
 		this.userRepository = userRepository;
 		this.deskRepository = deskRepository;
 		this.billingRepository = billingRepository;
@@ -60,26 +62,27 @@ class DefaultDemoDataIntoDB {
 	 */
 	private void generateMenuItems() {
 		LOGGER.info("Init demo menu item data");
+		MenuItemCategory category = menuItemCategoryRepository.findByName("categories");
 		MenuItem[] menu = {
-				new MenuItem(MenuItemCategory.APPETIZER, "kleiner Salat", 4.80, "EUR", "kalte Vorspeise", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.APPETIZER, "Bärlauchcremesuppe mit Räucherlachs", 4.80, "EUR", "warme Vorspeise", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.MAIN_COURSE, "Lachsfilet", 14.60, "EUR", "mit Tagliatelle und Tomaten", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.MAIN_COURSE, "Salatteller", 13.80, "EUR", "mit gebratenem Zanderfilet", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.MAIN_COURSE, "Pasta Bolognese", 11.90, "EUR", "mit Tomaten und Parmesan", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.MAIN_COURSE, "Schweinerückensteak", 13.90, "EUR", "mit Pfefferrahmsauce und Kartoffel Wedges", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.MAIN_COURSE, "Hausgemachte Kartoffel Gnocchi", 11.80, "EUR", "Gnocchi mit mediterranem Gemüse, Fetakäse, Rucola & Parmesan", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.MAIN_COURSE, "Hausgemachte Käsespätzle", 11.60, "EUR", "mit Beilagensalat", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DESSERT, "Drei Kugeln Eis", 6.10, "EUR", "mit Sahne", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Spezi", 2.10, "EUR", "0,20l", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Ginger Ale", 2.20, "EUR", "0,20l", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Warsteiner", 2.00, "EUR", "0.25l", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Guinness", 3.00, "EUR", "0.30l", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Cabernet Sauvignon", 3.90, "EUR", "0.20l", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Pinot Grigio", 3.90, "EUR", "0.20l", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Jägermeister", 2.00, "EUR", "5cl", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Sambuca", 3.90, "EUR", "5cl", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Tasse Kaffee", 1.50, "EUR", "mit Milch, Zucker", "", new ArrayList<>()),
-				new MenuItem(MenuItemCategory.DRINKS, "Heiße Schokolade", 2.10, "EUR", "mit Sahne", "", new ArrayList<>())
+				new MenuItem(category.getSubcategory().get(1).getSubcategory().get(1), "kleiner Salat", 4.80, "EUR", "kalte Vorspeise", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(1).getSubcategory().get(2), "Bärlauchcremesuppe mit Räucherlachs", 4.80, "EUR", "warme Vorspeise", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(2).getSubcategory().get(1), "Lachsfilet", 14.60, "EUR", "mit Tagliatelle und Tomaten", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(2).getSubcategory().get(1), "Salatteller", 13.80, "EUR", "mit gebratenem Zanderfilet", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(2).getSubcategory().get(2), "Pasta Bolognese", 11.90, "EUR", "mit Tomaten und Parmesan", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(2).getSubcategory().get(2), "Schweinerückensteak", 13.90, "EUR", "mit Pfefferrahmsauce und Kartoffel Wedges", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(2).getSubcategory().get(3), "Hausgemachte Kartoffel Gnocchi", 11.80, "EUR", "Gnocchi mit mediterranem Gemüse, Fetakäse, Rucola & Parmesan", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(2).getSubcategory().get(3), "Hausgemachte Käsespätzle", 11.60, "EUR", "mit Beilagensalat", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(3).getSubcategory().get(1), "Drei Kugeln Eis", 6.10, "EUR", "mit Sahne", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(1), "Spezi", 2.10, "EUR", "0,20l", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(1), "Ginger Ale", 2.20, "EUR", "0,20l", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(2), "Warsteiner", 2.00, "EUR", "0.25l", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(2), "Guinness", 3.00, "EUR", "0.30l", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(3), "Cabernet Sauvignon", 3.90, "EUR", "0.20l", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(3), "Pinot Grigio", 3.90, "EUR", "0.20l", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(4), "Jägermeister", 2.00, "EUR", "5cl", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(4), "Sambuca", 3.90, "EUR", "5cl", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(5), "Tasse Kaffee", 1.50, "EUR", "mit Milch, Zucker", "", new ArrayList<>()),
+				new MenuItem(category.getSubcategory().get(4).getSubcategory().get(5), "Heiße Schokolade", 2.10, "EUR", "mit Sahne", "", new ArrayList<>())
 		};
 		for(MenuItem m : menu) this.menuItems.add(menuItemRepository.save(m));
 	}
@@ -149,5 +152,98 @@ class DefaultDemoDataIntoDB {
 		orderItemRepository.save(orderItems);
 		LOGGER.info("Finished creating random orders");
 	}
+
+
+	private void generateMenuCategories(){
+		LOGGER.info("Generate menu Items");
+		MenuItemCategory category = new MenuItemCategory("Categories", new ArrayList<MenuItemCategory>(), null);
+		MenuItemCategory parent;
+		ArrayList<MenuItemCategory> tmp = new ArrayList<MenuItemCategory>();
+
+		// Appetizer
+		parent = category;
+		category = new MenuItemCategory("Appetizer", new ArrayList<MenuItemCategory>(), parent );
+		tmp = parent.getSubcategory();
+		tmp.add(category);
+		parent.setSubcategory(tmp);
+			//Warm-Appetizer
+			parent = category;
+			category = new MenuItemCategory("Warm_Appetizer", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+
+			//Cold-Appetizer
+			category = new MenuItemCategory("Cold_Appetizer", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+
+		// Main-Dish
+		parent = category.getParent().getParent();
+		category = new MenuItemCategory("Main_Dish", new ArrayList<MenuItemCategory>(), parent );
+		tmp = parent.getSubcategory();
+		tmp.add(category);
+		parent.setSubcategory(tmp);
+			//Fish-Dish
+			parent = category;
+			category = new MenuItemCategory("Fish", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+
+			//Meat
+			category = new MenuItemCategory("Meat", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+
+			//Plants
+			category = new MenuItemCategory("Vegetarian", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+		// Dessert
+		parent = category.getParent().getParent();
+		category = new MenuItemCategory("Dessert", new ArrayList<MenuItemCategory>(), parent );
+		tmp = parent.getSubcategory();
+		tmp.add(category);
+		parent.setSubcategory(tmp);
+		// Drinks
+		parent = category.getParent();
+		category = new MenuItemCategory("Drinks", new ArrayList<MenuItemCategory>(), parent );
+		tmp = parent.getSubcategory();
+		tmp.add(category);
+		parent.setSubcategory(tmp);
+			//Non-alk
+			parent = category;
+			category = new MenuItemCategory("Alc-free", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+
+			//Beer
+			category = new MenuItemCategory("Beer", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+
+			//Firewater
+			category = new MenuItemCategory("Highpercentage", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+
+			//Hot Drinks
+			category = new MenuItemCategory("HotDrinks", new ArrayList<MenuItemCategory>(), parent );
+			tmp = parent.getSubcategory();
+			tmp.add(category);
+			parent.setSubcategory(tmp);
+			while(parent.getParent() != null) parent = parent.getParent();
+
+		menuItemCategoryRepository.save(parent);
+
+	}
+
 
 }
