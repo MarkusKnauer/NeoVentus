7092ab33a73c347a37affede3e2212292ef3569d
Tim Heidelbach <tim@heidelba.ch>
2017-05-17 00:07:44 +0200

    Display username, roles and some mock data on profile page

--- frontend/src/pages/profile/profile.ts
+++ frontend/src/pages/profile/profile.ts
@@ -1,8 +1,10 @@
-import {Component} from "@angular/core";
-import {NavController} from "ionic-angular";
+import {Component, ViewChild} from "@angular/core";
+import {Content, NavController} from "ionic-angular";
+import {AuthGuardService} from "../../service/auth-guard.service";
 
 /**
- * @author Markus Knauer
+ * @author Markus Knauer, Tim Heidelbach
+ * @version 0.0.2 designed profile page
  * @version 0.0.1
  */
 
@@ -12,8 +14,61 @@ import {NavController} from "ionic-angular";
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController) {
+  private avatar;
+  private username = null;
+  private userroles: any = null;
 
+  private profit_today;
+  private profit_all;
+
+  private steps_today;
+  private steps_all;
+
+  @ViewChild(Content) content: Content;
+
+
+  constructor(public navCtrl: NavController, private authGuard: AuthGuardService) {
+
+    // TODO: make avatar dynamic
+    this.avatar = "../assets/avatars/default-male.png";
+
+    this.getUserName();
+    this.getUserRoles();
+
+    // TODO: get profit data
+    this.profit_today = 50.00;
+    this.profit_all = 20000.00;
+
+    // TODO: get steps data
+    this.steps_today = 500;
+    this.steps_all = 200000;
   }
 
+  getUserName() {
+    if (this.username == null) {
+      try {
+        this.username = this.authGuard.userDetails.name;
+      } catch (exception) {
+        console.error("Profile - Cannot read username");
+      }
+    }
+    return this.username;
+  }
+
+  getUserRoles() {
+    if (this.userroles == null) {
+      try {
+        this.userroles = this.authGuard.userDetails.authorities;
+
+      } catch (exception) {
+        console.error("Profile - Cannot read userroles");
+      }
+    }
+    return this.userroles;
+  }
+
+
+  fixDimensions() {
+    this.content.resize()
+  }
 }
