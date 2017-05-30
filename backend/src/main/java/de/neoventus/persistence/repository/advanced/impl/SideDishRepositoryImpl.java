ab5500ca88faeb1c642a3c358ecd9f21dcaa38eb
Tim H <tim@heidelba.ch>
2017-05-30 23:18:16 +0200

    Clean up

--- backend/src/main/java/de/neoventus/persistence/repository/advanced/impl/SideDishRepositoryImpl.java
+++ backend/src/main/java/de/neoventus/persistence/repository/advanced/impl/SideDishRepositoryImpl.java
@@ -1,32 +1,19 @@
-package de.neoventus.persistence.repository.advanced.impl;/**
- * Created by julian on 28.04.2017.
- */
+package de.neoventus.persistence.repository.advanced.impl;
 
 import de.neoventus.persistence.entity.MenuItem;
 import de.neoventus.persistence.entity.SideDishGroup;
 import de.neoventus.persistence.repository.MenuItemRepository;
-import de.neoventus.persistence.repository.SideDishRepository;
 import de.neoventus.persistence.repository.advanced.NVSideDishRepository;
 import de.neoventus.rest.dto.SideDishDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.util.logging.Logger;
-
 /**
  * @author: Julian Beck, Markus Knauer
- * @version: 0.0.2 edited (removed tree) MK
- * 			0.0.1 created by JB
- *
  **/
 public class SideDishRepositoryImpl implements NVSideDishRepository {
-	private final static Logger LOGGER = Logger.getLogger(SideDishRepositoryImpl.class.getName());
-
 
 	private MongoTemplate mongoTemplate;
-
-	@Autowired
-	private SideDishRepository sideDishRepository;
 
 	@Autowired
 	private MenuItemRepository menuItemRepository;
